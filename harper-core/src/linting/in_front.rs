e696c2034681c581be9685cc62aa121f8cc0d6e8
hippietrail <hippytrail@gmail.com>
2025-05-01 00:13:33 +0700

    refactor: `match_to_lint` logic easier than in-pattern

--- harper-core/src/linting/in_front.rs
+++ harper-core/src/linting/in_front.rs
@@ -1,6 +1,6 @@
 use crate::{
-    Lrc, Punctuation, Token,
-    patterns::{EitherPattern, Invert, Pattern, SequencePattern},
+    Lrc, Token,
+    patterns::{EitherPattern, Pattern, SequencePattern},
 };
 
 use super::{Lint, LintKind, PatternLinter, Suggestion};
@@ -13,24 +13,25 @@ impl Default for InFront {
     fn default() -> Self {
         let infront = Lrc::new(SequencePattern::default().t_aco("infront"));
 
-        let not_hyphen = Lrc::new(Invert::new(|t: &Token, _source: &[char]| {
-            t.kind.is_hyphen()
-        }));
-
-        let with_prev_not_hyphen = SequencePattern::default()
-            .then(not_hyphen.clone())
+        let with_prev = SequencePattern::default()
+            .then_anything()
             .then(infront.clone());
 
-        let with_next_not_hyphen = SequencePattern::default()
+        let with_next = SequencePattern::default()
             .then(infront.clone())
-            .then(not_hyphen.clone());
+            .then_anything();
+
+        let with_prev_and_next = SequencePattern::default()
+            .then_anything()
+            .then(infront.clone())
+            .then_anything();
 
         Self {
             pattern: Box::new(EitherPattern::new(vec![
-                // Box::new(infront),
-                Box::new(with_prev_not_hyphen),
-                // Box::new(with_next_tok),
-                Box::new(with_next_not_hyphen),
+                Box::new(with_prev_and_next),
+                Box::new(with_prev),
+                Box::new(with_next),
+                Box::new(infront),
             ])),
         }
     }
@@ -42,25 +43,34 @@ impl PatternLinter for InFront {
     }
 
     fn match_to_lint(&self, matched_toks: &[Token], source_chars: &[char]) -> Option<Lint> {
-        let span = matched_toks.first()?.span;
+        let infront_idx = get_infront_idx(matched_toks, source_chars);
+        let infront_span = matched_toks[infront_idx].span;
+        let infront_chars = infront_span.get_content(source_chars);
 
-        let chars = span.get_content(source_chars);
-
-        eprintln!(">>{}<<", chars.iter().collect::<String>());
-
-        // Ignore trademarks etc. like InFront, inFront
-        if (chars[0] == 'i' || chars[0] == 'I') && chars[1..] == ['n', 'F', 'r', 'o', 'n', 't'] {
+        // Ignore if there's a hyphen immediately on either side
+        if (0..matched_toks.len())
+            .filter(|&i| i != infront_idx)
+            .any(|i| matched_toks[i].kind.is_hyphen())
+        {
             return None;
         }
 
+        // Ignore trademarks etc. like InFront, inFront
+        if (infront_chars[0] == 'i' || infront_chars[0] == 'I')
+            && infront_chars[1..] == ['n', 'F', 'r', 'o', 'n', 't']
+        {
+            return None;
+        }
+
+        let template = infront_span.get_content(source_chars);
+
         Some(Lint {
-            span,
+            span: infront_span,
             lint_kind: LintKind::WordChoice,
             suggestions: vec![Suggestion::replace_with_match_case_str(
-                "in front",
-                span.get_content(source_chars),
+                "in front", template,
             )],
-            message: "Use `in front` rather than `of` here.".to_string(),
+            message: "`In front` should be written as two words.".to_string(),
             priority: 127,
         })
     }
@@ -70,13 +80,56 @@ impl PatternLinter for InFront {
     }
 }
 
+fn get_infront_idx(toks: &[Token], src: &[char]) -> usize {
+    const LEN: usize = "infront".len();
+
+    if toks.len() != 1 && toks.len() != 2 && toks.len() != 3 {
+        unreachable!();
+    }
+
+    // we matched some lettercase variant of `infront` on its own
+    if toks.len() == 1 {
+        return 0;
+    }
+
+    // we matched `infront` with a non-hyphen token on one side
+    if toks.len() == 3 {
+        return 1;
+    }
+
+    // we matched a non-hyphen token on one side of `infront`
+    let len0 = toks[0].span.len();
+    let len1 = toks[1].span.len();
+
+    if len0 == LEN && len1 != LEN {
+        return 0;
+    } else if len1 == LEN && len0 != LEN {
+        return 1;
+    }
+
+    // both tokens the right length, check kind
+    if toks[0].kind.is_word() && !toks[1].kind.is_word() {
+        return 0;
+    } else if !toks[0].kind.is_word() && toks[1].kind.is_word() {
+        return 1;
+    }
+
+    // both tokens: words the right length, check the chars
+    return !toks[0]
+        .span
+        .get_content(src)
+        .iter()
+        .collect::<String>()
+        .eq_ignore_ascii_case("infront") as usize;
+}
+
 #[cfg(test)]
 mod tests {
     use super::InFront;
     use crate::linting::tests::{assert_lint_count, assert_suggestion_result};
 
     #[test]
-    fn corrects_lone_incase() {
+    fn corrects_lone_infront() {
         assert_suggestion_result(
             "Button always overlaps (infront) of other views.",
             InFront::default(),
@@ -111,16 +164,6 @@ mod tests {
         );
     }
 
-    // it's actually the apostrophe that makes this a valid word
-    // #[test]
-    // fn ignore_with_hyphen_before() {
-    //     assert_lint_count(
-    //         "GitHub Gist: star and fork yossi-infront's gists by creating an account on GitHub.",
-    //         InFront::default(),
-    //         0,
-    //     );
-    // }
-
     #[test]
     fn correct_with_period_after() {
         assert_suggestion_result(
@@ -128,6 +171,16 @@ mod tests {
             InFront::default(),
             "Car with a reversed ramp in front.",
         );
+    }
+
+    #[test]
+    fn ignore_hyphen_before() {
+        assert_lint_count("-infront", InFront::default(), 0);
+    }
+
+    #[test]
+    fn ignore_hyphen_after() {
+        assert_lint_count("infront-", InFront::default(), 0);
     }
 
     #[test]
@@ -143,9 +196,13 @@ mod tests {
     fn ignores_with_hyphen_after() {
         assert_lint_count(
             "infront-cycle.ipe · infront-cycle.ipe · infront-cycle.svg · infront-cycle.svg · infront-s1s2.ipe · infront-s1s2.ipe · infront-s1s2.svg · infront-s1s2.svg.",
-            // "infront-cycle.ipe",
             InFront::default(),
             0,
         );
+    }
+
+    #[test]
+    fn even_repeated_infront_works() {
+        assert_suggestion_result("infront infront", InFront::default(), "in front in front");
     }
 }
