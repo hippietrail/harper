b1cf7547093851bdd8deb2929c487d848fab35d9
hippietrail <hippytrail@gmail.com>
2025-07-12 20:32:17 +0900

    fix: don't flag `.exe` as space before full stop
    Solves #1548

--- harper-core/src/linting/spaces.rs
+++ harper-core/src/linting/spaces.rs
@@ -8,8 +8,11 @@ pub struct Spaces;
 impl Linter for Spaces {
     fn lint(&mut self, document: &Document) -> Vec<Lint> {
         let mut output = Vec::new();
+        let mut sentences = document.iter_sentences().peekable();
 
-        for sentence in document.iter_sentences() {
+        while let Some(sentence) = sentences.next() {
+            let maybe_next_sentence = sentences.peek();
+
             for space in sentence.iter_spaces() {
                 let TokenKind::Space(count) = space.kind else {
                     panic!("The space iterator should only return spaces.")
@@ -46,15 +49,17 @@ impl Linter for Spaces {
                     }
                 ]
             ) {
-                output.push(Lint {
-                    span: sentence[sentence.len() - 2..sentence.len() - 1]
-                        .span()
-                        .unwrap(),
-                    lint_kind: LintKind::Formatting,
-                    suggestions: vec![Suggestion::Remove],
-                    message: "Unnecessary space at the end of the sentence.".to_string(),
-                    priority: 63,
-                })
+                if !is_file_extension(document, sentence, maybe_next_sentence) {
+                    output.push(Lint {
+                        span: sentence[sentence.len() - 2..sentence.len() - 1]
+                            .span()
+                            .unwrap(),
+                        lint_kind: LintKind::Formatting,
+                        suggestions: vec![Suggestion::Remove],
+                        message: "Unnecessary space at the end of the sentence.".to_string(),
+                        priority: 63,
+                    });
+                }
             }
         }
 
@@ -64,6 +69,29 @@ impl Linter for Spaces {
     fn description(&self) -> &'static str {
         "Words should be separated by at most one space."
     }
+}
+
+fn is_file_extension(
+    document: &Document,
+    sentence: &[Token],
+    maybe_next_sentence: Option<&&[Token]>,
+) -> bool {
+    if !sentence.last().unwrap().kind.is_period() {
+        return false;
+    }
+
+    let next_sentence = match maybe_next_sentence {
+        Some(s) => s,
+        None => return false,
+    };
+
+    let first_token = match next_sentence.first() {
+        Some(t) if t.kind.is_word() && t.span.len() == 3 => t,
+        _ => return false,
+    };
+
+    let chars = first_token.span.get_content(&document.get_source());
+    chars.iter().all(|c| c.is_ascii_lowercase()) || chars.iter().all(|c| c.is_ascii_uppercase())
 }
 
 #[cfg(test)]
@@ -81,6 +109,62 @@ mod tests {
     #[test]
     fn allows_period_without_space() {
         let source = "There isn't a space at the end of this sentence.";
+
+        assert_lint_count(source, Spaces, 0)
+    }
+
+    #[test]
+    fn doesnt_flag_file_extensions() {
+        let source = "Windows users can download an .exe file.";
+
+        assert_lint_count(source, Spaces, 0)
+    }
+
+    #[test]
+    fn doesnt_flag_caps_file_extensions() {
+        let source = "DOS users can download an .exe file.";
+
+        assert_lint_count(source, Spaces, 0)
+    }
+
+    #[test]
+    fn flags_non_file_extensions() {
+        let source = "None of .xx or .abcd or .Xyz or .ab1 are acceptable as file extensions.";
+
+        assert_lint_count(source, Spaces, 4)
+    }
+
+    #[test]
+    fn doesnt_flag_file_extensions_inside_markdown_link() {
+        let source = "Windows users can [download an .exe file](https://yt-dl.org/latest/youtube-dl.exe) and stuff.";
+
+        assert_lint_count(source, Spaces, 0)
+    }
+
+    #[test]
+    fn doesnt_flag_file_extensions_inside_markdown_but_flag_ellipsis_after_space() {
+        let source = "Windows users can [download an .exe file](https://yt-dl.org/latest/youtube-dl.exe) and ...";
+
+        assert_lint_count(source, Spaces, 1)
+    }
+
+    #[test]
+    fn flags_file_extensions_after_question_mark() {
+        let source = "So ?exe won't work.";
+
+        assert_lint_count(source, Spaces, 1)
+    }
+
+    #[test]
+    fn flags_file_extensions_after_exclamation_mark() {
+        let source = "And !COM won't work either.";
+
+        assert_lint_count(source, Spaces, 1)
+    }
+
+    #[test]
+    fn doesnt_flag_sentence_ending_with_ellipsis() {
+        let source = "Is this gonna work...";
 
         assert_lint_count(source, Spaces, 0)
     }
