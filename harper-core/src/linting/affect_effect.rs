9890399b0a28af61ec90358f26b34df7b6366fbc
hippietrail <hippytrail@gmail.com>
2025-08-28 18:10:58 +0900

    chore: more legit sentences

--- harper-core/src/linting/affect_effect.rs
+++ harper-core/src/linting/affect_effect.rs
@@ -120,13 +120,68 @@ mod tests {
         assert_no_lints("How do null values affect performance?", AffectEffect);
     }
 
-    // legit affects - verb 3rd person singular
-    
+    #[test]
+    fn dont_flag_only_affect_things_that_use_schannel_ssp() {
+        assert_no_lints(
+            "Changes made by this category only affect things that use [Schannel SSP]",
+            AffectEffect,
+        );
+    }
+
+    #[test]
+    fn dont_flag_that_can_affect_performance() {
+        assert_no_lints(
+            "you might want to be aware of the options that can affect performance",
+            AffectEffect,
+        );
+    }
+
+    #[test]
+    fn dont_flag_can_sometimes_affect_gameplay_too() {
+        assert_no_lints(
+            "but can sometimes affect gameplay too",
+            AffectEffect::default(),
+        );
+    }
+
+    // legit affected - verb - past participle
+
+    #[test]
+    fn dont_flag_is_not_affected() {
+        assert_no_lints(
+            "Local RDP such as for Hyper-V enhanced session is not affected.",
+            AffectEffect,
+        );
+    }
+
+    #[test]
+    fn dont_flag_the_people_who_were_affected_by_this_tragedy() {
+        assert_no_lints(
+            "your help will be immensely valuable for the people who were affected by this tragedy",
+            AffectEffect::default(),
+        );
+    }
+
+    // legit affects - verb - 3rd person singular
+
     #[test]
     fn dont_flag_this_value_directly_affects() {
-        assert_no_lints("This value directly affects the execution time of this workflow", AffectEffect);
+        assert_no_lints(
+            "This value directly affects the execution time of this workflow",
+            AffectEffect,
+        );
     }
-    
+
+    // legit affecting - verb - present participle
+
+    #[test]
+    fn dont_flag_vulnerabilities_affecting_microsoft_products() {
+        assert_no_lints(
+            "The Microsoft Security Response Center (MSRC) investigates all reports of security vulnerabilities affecting Microsoft products and services",
+            AffectEffect,
+        );
+    }
+
     // legit effect - noun - plural
 
     #[test]
@@ -141,4 +196,137 @@ mod tests {
             AffectEffect,
         );
     }
+
+    #[test]
+    fn dont_flag_not_have_any_effect_on() {
+        assert_no_lints("They do not have any effect on security.", AffectEffect);
+    }
+
+    #[test]
+    fn dont_flag_wont_have_any_effect_on_you() {
+        assert_no_lints(
+            "seeing the source code won't have any effect on you because you aren't able to understand nor verify it.",
+            AffectEffect,
+        );
+    }
+
+    #[test]
+    fn dont_flag_will_have_no_effect() {
+        assert_no_lints(
+            "Setting the variables after sourcing the script will have no effect",
+            AffectEffect,
+        );
+    }
+
+    #[test]
+    fn dont_flag_if_a_step_causes_an_effect_to_be_executed() {
+        assert_no_lints(
+            "if a step causes an effect to be executed",
+            AffectEffect::default(),
+        );
+    }
+
+    #[test]
+    fn dont_flag_an_effect_that_reaches_out() {
+        assert_no_lints(
+            "Currently our reducer is using an effect that reaches out into the real world to hit an API server",
+            AffectEffect::default(),
+        );
+    }
+
+    #[test]
+    fn dont_flag_take_effect() {
+        assert_no_lints(
+            "please redeploy the project for the changes to take effect.",
+            AffectEffect::default(),
+        );
+    }
+
+    // legit effects - noun - plural
+
+    #[test]
+    fn dont_flag_side_effects() {
+        assert_no_lints(
+            "side effects influence your application",
+            AffectEffect::default(),
+        );
+    }
+
+    #[test]
+    fn dont_flag_run_the_reducer_and_effects() {
+        assert_no_lints(
+            "so that the store can run the reducer and effects, and you can observe state changes in the store",
+            AffectEffect::default(),
+        );
+    }
+
+    #[test]
+    fn dont_flag_any_effects_that_should() {
+        assert_no_lints(
+            "The reducer is also responsible for returning any effects that should be",
+            AffectEffect::default(),
+        );
+    }
+
+    #[test]
+    fn and_what_effects_need_to_be_executed() {
+        assert_no_lints(
+            "current state to the next state, and what effects need to be executed",
+            AffectEffect::default(),
+        );
+    }
+
+    #[test]
+    fn dont_flag_execute_effects() {
+        assert_no_lints(
+            "execute effects, and they can return `.none`",
+            AffectEffect::default(),
+        );
+    }
+
+    #[test]
+    fn dont_flag_including_the_effects_without() {
+        assert_no_lints(
+            "And we can immediately test this logic, including the effects, without",
+            AffectEffect::default(),
+        );
+    }
+
+    #[test]
+    fn dont_flag_specialized_effects_and_transformations() {
+        assert_no_lints(
+            "Create specialized effects and transformations for video generation",
+            AffectEffect::default(),
+        );
+    }
+
+    // legit seem - verb - lemma
+
+    #[test]
+    fn dont_flag_something_doesnt_seem_right() {
+        assert_no_lints(
+            "If you find something which doesn't make sense, or something doesn't seem right, please make a pull request",
+            AffectEffect,
+        );
+    }
+
+    // legit seems - verb - 3rd person singular
+
+    #[test]
+    fn dont_flag_it_seems_to_be_able_to() {
+        assert_no_lints(
+            "it seems to be able to handle a bit of general tasks such as",
+            AffectEffect,
+        );
+    }
+
+    #[test]
+    fn dont_flag_what_seems_like_a_bug_might_be() {
+        assert_no_lints(
+            "What seems like a bug might be intended behaviour.",
+            AffectEffect::default(),
+        );
+    }
+
+    //////////////////////////////
 }
