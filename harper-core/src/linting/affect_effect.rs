4d98daa6fa4eb6ac8dad718e562e2e2b99374348
hippietrail <hippytrail@gmail.com>
2025-09-03 01:11:58 +0900

    chore: experimenting with POS methods

--- harper-core/src/linting/affect_effect.rs
+++ harper-core/src/linting/affect_effect.rs
@@ -3,6 +3,92 @@ use crate::{
     linting::{Lint, LintKind, Linter, Suggestion},
 };
 
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq)]
+pub struct PosStruct {
+    pub c: bool,  // conjunction
+    pub i: bool,  // pronoun (I)
+    pub d: bool,  // determiner
+    pub j: bool,  // adjective
+    pub n: bool,  // noun
+    pub p: bool,  // preposition
+    pub r: bool,  // adverb (R for adverb, since A is already used for adjective)
+    pub v: bool,  // verb
+}
+
+impl PosStruct {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn to_string(&self) -> String {
+        let mut s = String::with_capacity(8);
+        if self.v { s.push('V'); }
+        if self.n { s.push('N'); }
+        if self.r { s.push('R'); }
+        if self.j { s.push('J'); }
+        if self.p { s.push('P'); }
+        if self.i { s.push('I'); }
+        if self.d { s.push('D'); }
+        if self.c { s.push('C'); }
+        s
+    }
+    
+    // Bitwise OR operation
+    pub fn or(self, other: Self) -> Self {
+        Self {
+            c: self.c || other.c,
+            i: self.i || other.i,
+            d: self.d || other.d,
+            j: self.j || other.j,
+            n: self.n || other.n,
+            p: self.p || other.p,
+            r: self.r || other.r,
+            v: self.v || other.v,
+        }
+    }
+    
+    // Bitwise AND operation
+    pub fn and(self, other: Self) -> Self {
+        Self {
+            c: self.c && other.c,
+            i: self.i && other.i,
+            d: self.d && other.d,
+            j: self.j && other.j,
+            n: self.n && other.n,
+            p: self.p && other.p,
+            r: self.r && other.r,
+            v: self.v && other.v,
+        }
+    }
+    
+    // Check if any of the flags are set
+    pub fn any(self) -> bool {
+        self.c || self.i || self.d || self.j || self.n || self.p || self.r || self.v
+    }
+    
+    // Check if all flags are set
+    pub fn all(self) -> bool {
+        self.c && self.i && self.d && self.j && self.n && self.p && self.r && self.v
+    }
+}
+
+// Implement BitOr and BitAnd for nice syntax
+impl std::ops::BitOr for PosStruct {
+    type Output = Self;
+    
+    fn bitor(self, rhs: Self) -> Self::Output {
+        self.or(rhs)
+    }
+}
+
+impl std::ops::BitAnd for PosStruct {
+    type Output = Self;
+    
+    fn bitand(self, rhs: Self) -> Self::Output {
+        self.and(rhs)
+    }
+}
+
 #[derive(Debug, Default)]
 pub struct AffectEffect;
 
@@ -55,15 +141,34 @@ impl Linter for AffectEffect {
                     _ => continue,
                 };
 
-                eprintln!(
-                    "❤️ '{}'",
-                    tok.span.get_content_string(document.get_source())
-                );
-
                 let toks = chunk.widen_slice(wix, 2);
 
-                if let Some(span) = toks.span() {
-                    eprintln!("❤️ '{}'", span.get_content_string(document.get_source()));
+                if toks.len() == 5 {
+                    let first_tok = toks.first().and_then(|t| t.kind.as_word());
+                    let last_tok = toks.last().and_then(|t| t.kind.as_word());
+
+                    if let (Some(Some(first)), Some(Some(last))) = (first_tok, last_tok) {
+                        let first_txt = tokpos(first); //format!("{:#?}", first);
+                        let last_txt = tokpos(last); //format!("{:#?}", last);
+                        // eprintln!("{}", first_txt);
+                        if let Some(span) = toks.span() {
+                            eprintln!(
+                                "❤️ {} '{}' {}",
+                                first_txt,
+                                span.get_content_string(document.get_source()),
+                                last_txt
+                            );
+                        }
+                        // eprintln!("{}", last_txt);
+                    }
+                } else {
+                    if let Some(span) = toks.span() {
+                        eprintln!(
+                            "💔 [{}] {}",
+                            toks.len(),
+                            span.get_content_string(document.get_source())
+                        );
+                    }
                 }
 
                 let ending = &tok.span.get_content(document.get_source())[stem_len..];
@@ -95,238 +200,227 @@ impl Linter for AffectEffect {
     }
 }
 
+use crate::WordMetadata;
+
+fn tokpos(tok: &WordMetadata) -> PosStruct {
+    // build a string such as VNP (meaning verb & noun & preposition)
+    let mut pos = PosStruct::new();
+    if tok.is_conjunction() {
+        pos.c = true;
+    }
+    if tok.is_pronoun() {
+        pos.i = true;
+    }
+    if tok.is_determiner() {
+        pos.d = true;
+    }
+    if tok.is_adjective() {
+        pos.j = true;
+    }
+    if tok.is_noun() {
+        pos.n = true;
+    }
+    if tok.preposition {
+        pos.p = true;
+    }
+    if tok.is_adverb() {
+        pos.r = true;
+    }
+    if tok.is_verb() {
+        pos.v = true;
+    }
+    pos
+}
+
 #[cfg(test)]
 mod tests {
     use super::AffectEffect;
-    use crate::linting::tests::{assert_lint_count, assert_no_lints};
 
     // legit affect - verb - lemma
 
     #[test]
-    fn dont_flag_every_code_change_might_affect_anything_else() {
-        assert_no_lints("every code change might affect anything else", AffectEffect);
-    }
+    fn all_legit_affect_sentences() {
+        use crate::Document;
+        use crate::linting::Linter;
 
-    #[test]
-    fn dont_flag_how_these_affect_the_ux() {
-        assert_no_lints(
+        let sentences = [
+            "every code change might affect anything else",
             "probably you’ll never be aware which are your slowest code parts under real-world scenario and how these affect the UX",
-            AffectEffect,
-        );
-    }
-
-    #[test]
-    fn dont_flag_null_values_affect_performance() {
-        assert_no_lints("How do null values affect performance?", AffectEffect);
-    }
-
-    #[test]
-    fn dont_flag_only_affect_things_that_use_schannel_ssp() {
-        assert_no_lints(
-            "Changes made by this category only affect things that use [Schannel SSP]",
-            AffectEffect,
-        );
-    }
-
-    #[test]
-    fn dont_flag_that_can_affect_performance() {
-        assert_no_lints(
+            "How do null values affect performance?",
+            "changes made by this category only affect things that use [Schannel SSP]",
             "you might want to be aware of the options that can affect performance",
-            AffectEffect,
-        );
+            "but can sometimes affect gameplay too",
+            "Preview how CSS3 border-radius values affect an element",
+            "This modification may temporarily affect network connectivity",
+            "Modifying this registry key may affect:",
+        ];
+
+        for s in sentences {
+            AffectEffect.lint(&Document::new_markdown_default_curated(s));
+        }
     }
 
     #[test]
-    fn dont_flag_can_sometimes_affect_gameplay_too() {
-        assert_no_lints(
-            "but can sometimes affect gameplay too",
-            AffectEffect::default(),
-        );
+    fn all_bad_affect_sentences() {
+        use crate::Document;
+        use crate::linting::Linter;
+
+        let sentences = [
+            "Since, raw response is the only option for unstable search commands Val() and Result() calls wouldn't have any affect on them:",
+        ];
+
+        for s in sentences {
+            AffectEffect.lint(&Document::new_markdown_default_curated(s));
+        }
     }
 
     // legit affected - verb - past participle
 
     #[test]
-    fn dont_flag_is_not_affected() {
-        assert_no_lints(
-            "Local RDP such as for Hyper-V enhanced session is not affected.",
-            AffectEffect,
-        );
-    }
+    fn all_legit_affected_sentences() {
+        use crate::Document;
+        use crate::linting::Linter;
 
-    #[test]
-    fn dont_flag_the_people_who_were_affected_by_this_tragedy() {
-        assert_no_lints(
+        let sentences = [
+            "Local RDP such as for Hyper-V enhanced session is not affected.",
             "your help will be immensely valuable for the people who were affected by this tragedy",
-            AffectEffect::default(),
-        );
+        ];
+
+        for s in sentences {
+            AffectEffect.lint(&Document::new_markdown_default_curated(s));
+        }
     }
 
     // legit affects - verb - 3rd person singular
 
     #[test]
-    fn dont_flag_this_value_directly_affects() {
-        assert_no_lints(
+    fn all_legit_affects_sentences() {
+        use crate::Document;
+        use crate::linting::Linter;
+
+        let sentences = [
             "This value directly affects the execution time of this workflow",
-            AffectEffect,
-        );
+            "This affects me as well, with version 2015.01.01.",
+        ];
+
+        for s in sentences {
+            AffectEffect.lint(&Document::new_markdown_default_curated(s));
+        }
     }
 
     // legit affecting - verb - present participle
 
     #[test]
-    fn dont_flag_vulnerabilities_affecting_microsoft_products() {
-        assert_no_lints(
+    fn all_legit_affecting_sentences() {
+        use crate::Document;
+        use crate::linting::Linter;
+
+        let sentences = [
             "The Microsoft Security Response Center (MSRC) investigates all reports of security vulnerabilities affecting Microsoft products and services",
-            AffectEffect,
-        );
-    }
+        ];
 
-    // legit effect - noun - plural
-
-    #[test]
-    fn dont_flag_avoid_effects_outside_of_functions() {
-        assert_no_lints("Avoid effects outside of functions.", AffectEffect);
-    }
-
-    #[test]
-    fn dont_flag_code_with_effects_like_network_or_db_calls() {
-        assert_no_lints(
-            "Avoid putting code with effects like network or DB calls outside of functions.",
-            AffectEffect,
-        );
-    }
-
-    #[test]
-    fn dont_flag_not_have_any_effect_on() {
-        assert_no_lints("They do not have any effect on security.", AffectEffect);
-    }
-
-    #[test]
-    fn dont_flag_wont_have_any_effect_on_you() {
-        assert_no_lints(
-            "seeing the source code won't have any effect on you because you aren't able to understand nor verify it.",
-            AffectEffect,
-        );
-    }
-
-    #[test]
-    fn dont_flag_will_have_no_effect() {
-        assert_no_lints(
-            "Setting the variables after sourcing the script will have no effect",
-            AffectEffect,
-        );
-    }
-
-    #[test]
-    fn dont_flag_if_a_step_causes_an_effect_to_be_executed() {
-        assert_no_lints(
-            "if a step causes an effect to be executed",
-            AffectEffect::default(),
-        );
-    }
-
-    #[test]
-    fn dont_flag_an_effect_that_reaches_out() {
-        assert_no_lints(
-            "Currently our reducer is using an effect that reaches out into the real world to hit an API server",
-            AffectEffect::default(),
-        );
-    }
-
-    #[test]
-    fn dont_flag_take_effect() {
-        assert_no_lints(
-            "please redeploy the project for the changes to take effect.",
-            AffectEffect::default(),
-        );
+        for s in sentences {
+            AffectEffect.lint(&Document::new_markdown_default_curated(s));
+        }
     }
 
     // legit effects - noun - plural
 
     #[test]
-    fn dont_flag_side_effects() {
-        assert_no_lints(
+    fn all_legit_effects_sentences() {
+        use crate::Document;
+        use crate::linting::Linter;
+
+        let sentences = [
+            "Avoid effects outside of functions.",
+            "Avoid putting code with effects like network or DB calls outside of functions.",
             "side effects influence your application",
-            AffectEffect::default(),
-        );
-    }
-
-    #[test]
-    fn dont_flag_run_the_reducer_and_effects() {
-        assert_no_lints(
             "so that the store can run the reducer and effects, and you can observe state changes in the store",
-            AffectEffect::default(),
-        );
-    }
-
-    #[test]
-    fn dont_flag_any_effects_that_should() {
-        assert_no_lints(
             "The reducer is also responsible for returning any effects that should be",
-            AffectEffect::default(),
-        );
-    }
-
-    #[test]
-    fn and_what_effects_need_to_be_executed() {
-        assert_no_lints(
             "current state to the next state, and what effects need to be executed",
-            AffectEffect::default(),
-        );
-    }
-
-    #[test]
-    fn dont_flag_execute_effects() {
-        assert_no_lints(
             "execute effects, and they can return `.none`",
-            AffectEffect::default(),
-        );
-    }
-
-    #[test]
-    fn dont_flag_including_the_effects_without() {
-        assert_no_lints(
             "And we can immediately test this logic, including the effects, without",
-            AffectEffect::default(),
-        );
+            "Create specialized effects and transformations for video generation",
+        ];
+
+        for s in sentences {
+            AffectEffect.lint(&Document::new_markdown_default_curated(s));
+        }
     }
 
+    // legit effect - noun - singular
+
     #[test]
-    fn dont_flag_specialized_effects_and_transformations() {
-        assert_no_lints(
-            "Create specialized effects and transformations for video generation",
-            AffectEffect::default(),
-        );
+    fn all_legit_effect_sentences() {
+        use crate::Document;
+        use crate::linting::Linter;
+
+        let sentences = [
+            "They do not have any effect on security.",
+            "seeing the source code won't have any effect on you because you aren't able to understand nor verify it.",
+            "Setting the variables after sourcing the script will have no effect",
+            "if a step causes an effect to be executed",
+            "Currently our reducer is using an effect that reaches out into the real world to hit an API server",
+            "please redeploy the project for the changes to take effect.",
+            "in effect, forming one big layer",
+        ];
+
+        for s in sentences {
+            AffectEffect.lint(&Document::new_markdown_default_curated(s));
+        }
     }
 
     // legit seem - verb - lemma
 
     #[test]
-    fn dont_flag_something_doesnt_seem_right() {
-        assert_no_lints(
+    fn all_legit_seem_sentences() {
+        use crate::Document;
+        use crate::linting::Linter;
+
+        let sentences = [
             "If you find something which doesn't make sense, or something doesn't seem right, please make a pull request",
-            AffectEffect,
-        );
+            "Ubuntu packages do not seem to get updated anymore.",
+            "Some of our users seem to think there is a limit of issues they can or should open.",
+            "Do not post features because they seem like a good idea.",
+        ];
+
+        for s in sentences {
+            AffectEffect.lint(&Document::new_markdown_default_curated(s));
+        }
     }
 
     // legit seems - verb - 3rd person singular
 
     #[test]
-    fn dont_flag_it_seems_to_be_able_to() {
-        assert_no_lints(
+    fn all_legit_seems_sentences() {
+        use crate::Document;
+        use crate::linting::Linter;
+
+        let sentences = [
             "it seems to be able to handle a bit of general tasks such as",
-            AffectEffect,
-        );
+            "What seems like a bug might be intended behaviour.",
+            "Even if it seems self-evident, please",
+            "(seems discontinued)",
+        ];
+
+        for s in sentences {
+            AffectEffect.lint(&Document::new_markdown_default_curated(s));
+        }
     }
+
+    // legit seemed - verb - past
 
     #[test]
-    fn dont_flag_what_seems_like_a_bug_might_be() {
-        assert_no_lints(
-            "What seems like a bug might be intended behaviour.",
-            AffectEffect::default(),
-        );
-    }
+    fn all_legit_seemed_sentences() {
+        use crate::Document;
+        use crate::linting::Linter;
 
-    //////////////////////////////
+        let sentences = [
+            "These libraries were placed here because none of the other categories seemed to fit.",
+        ];
+
+        for s in sentences {
+            AffectEffect.lint(&Document::new_markdown_default_curated(s));
+        }
+    }
 }
