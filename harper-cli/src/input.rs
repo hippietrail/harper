24edc0cb85eef0427f74eced53e74b5123c157c7
Andrew Dunbar <hippytrail@gmail.com>
2025-11-21 23:14:23 +0800

    Harper cli lint many (#2138)
    * feat: `harper-cli lint`: multiple args including dirs
    
    * fix: needs cargo.lock
    
    * chore: fmt
    
    * fix: remove comments and commented-out code
    
    * merge upstream
    
    * refactor: move the `lint` command to its own file

--- harper-cli/src/input.rs
+++ harper-cli/src/input.rs
@@ -12,20 +12,33 @@ use harper_core::{
 pub(super) enum Input {
     /// File (path) input.
     File(PathBuf),
+    /// Directory (path) input.
+    Dir(PathBuf),
     /// Direct text input, via the command line.
     Text(String),
+    /// Standard input (stdin).
+    Stdin(String),
 }
 impl Input {
     /// Loads the contained file/string into a conventional format. Returns a `Result` containing
     /// a tuple of a `Document` and its corresponding source text as a string.
     pub(super) fn load(
         &self,
+        batch_mode: bool,
         markdown_options: MarkdownOptions,
         dictionary: &impl Dictionary,
-    ) -> anyhow::Result<(Document, String)> {
+    ) -> anyhow::Result<(Option<Document>, String)> {
         match self {
-            Input::File(file) => super::load_file(file, markdown_options, dictionary),
-            Input::Text(s) => Ok((Document::new(s, &PlainEnglish, dictionary), s.clone())),
+            Input::File(file) => super::load_file(
+                file,
+                Some(&self.get_identifier()),
+                batch_mode,
+                markdown_options,
+                dictionary,
+            ),
+            Input::Dir(dir) => Ok((None, dir.display().to_string())),
+            Input::Text(s) => Ok((Some(Document::new(s, &PlainEnglish, dictionary)), s.clone())),
+            Input::Stdin(s) => Ok((Some(Document::new(s, &PlainEnglish, dictionary)), s.clone())),
         }
     }
 
@@ -37,7 +50,11 @@ impl Input {
             Input::File(file) => file
                 .file_name()
                 .map_or(Cow::from("<file>"), |file_name| file_name.to_string_lossy()),
-            Input::Text(_) => Cow::from("<input>"),
+            Input::Dir(dir) => dir
+                .file_name()
+                .map_or(Cow::from("<dir>"), |dir_name| dir_name.to_string_lossy()),
+            Input::Text(_) => Cow::from("<text>"),
+            Input::Stdin(_) => Cow::from("<stdin>"),
         }
     }
 
@@ -46,7 +63,7 @@ impl Input {
     pub(super) fn try_from_stdin() -> anyhow::Result<Self> {
         let mut buf = String::new();
         std::io::stdin().lock().read_to_string(&mut buf)?;
-        Ok(Self::from(buf))
+        Ok(Self::Stdin(buf))
     }
 }
 // This allows this type to be directly used with clap as an argument.
@@ -60,6 +77,11 @@ impl From<String> for Input {
         {
             // Input is a valid file path.
             Self::File(input_string.into())
+        } else if let Ok(metadata) = std::fs::metadata(&input_string)
+            && metadata.is_dir()
+        {
+            // Input is a valid directory path.
+            Self::Dir(input_string.into())
         } else {
             // Input is not a valid file path, we assume it's intended to be a string.
             Self::Text(input_string)
