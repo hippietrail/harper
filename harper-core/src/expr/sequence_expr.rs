90efaf64305d4d9c7b62e710c6fd2b3a531f1407
Andrew Dunbar <hippytrail@gmail.com>
2025-11-28 15:37:58 +0000

    fix: don't flag 'every day free' (#2260)

--- harper-core/src/expr/sequence_expr.rs
+++ harper-core/src/expr/sequence_expr.rs
@@ -420,6 +420,7 @@ impl SequenceExpr {
 
     gen_then_from_is!(noun);
     gen_then_from_is!(proper_noun);
+    gen_then_from_is!(plural_noun);
     gen_then_from_is!(mass_noun_only);
 
     // Pronouns
