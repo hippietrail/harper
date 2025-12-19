a5ae1afef85181e02dc308cd4dfb9cff89e478a4
Elijah Potter <me@elijahpotter.dev>
2025-12-19 13:05:55 -0700

    fix(chrome-ext): set global timeout

--- packages/chrome-plugin/playwright.config.ts
+++ packages/chrome-plugin/playwright.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
 	},
+	globalTimeout: 120000,
 	webServer: {
 		command: 'pnpm http-server ./tests/pages/ -p 8081',
 		url: 'http://127.0.0.1:8081',
