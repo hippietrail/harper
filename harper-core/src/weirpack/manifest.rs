862a9b17f1998b2e3f6d5e94a5719787b1a38cb1
Elijah Potter <me@elijahpotter.dev>
2026-02-04 12:07:16 -0700

    refactor(core): use a macro and document the methods (#2655)

--- harper-core/src/weirpack/manifest.rs
+++ harper-core/src/weirpack/manifest.rs
@@ -1,82 +1,78 @@
 use std::io::{Read, Write};
 
 use hashbrown::HashMap;
+use paste::paste;
 use serde_json::Value;
 
-use super::WeirpackError;
+use super::Error;
 
 #[derive(Debug, Clone, Default)]
 pub struct WeirpackManifest {
     data: HashMap<String, Value>,
 }
 
+macro_rules! gen_fns {
+    ($field:ident) => {
+        paste! {
+            #[doc = concat!("Get the ", stringify!($field), " field from the manifest.")]
+            pub fn $field(&self) -> Result<&str, Error>{
+                self.required_str(stringify!($field))
+            }
+
+            #[doc = concat!("Set the ", stringify!($field), " field in the manifest.")]
+            pub fn [< set_ $field >](&mut self, value: impl Into<String>) {
+                self.set_field(stringify!($field), Value::String(value.into()));
+            }
+        }
+    };
+}
+
 impl WeirpackManifest {
+    /// Create a new, empty manifest.
+    /// Note that an empty manifest is an invalid manifest.
     pub fn new() -> Self {
         Self::default()
     }
 
-    pub fn from_reader(reader: impl Read) -> Result<Self, WeirpackError> {
+    /// Load a manifest from some bytes.
+    pub fn from_reader(reader: impl Read) -> Result<Self, Error> {
         let data: HashMap<String, Value> = serde_json::from_reader(reader)?;
         let manifest = Self { data };
         manifest.validate_required()?;
         Ok(manifest)
     }
 
-    pub fn write_to(&self, writer: impl Write) -> Result<(), WeirpackError> {
+    /// Write a manifest to some bytes.
+    pub fn write_to(&self, writer: impl Write) -> Result<(), Error> {
         self.validate_required()?;
         serde_json::to_writer_pretty(writer, &self.data)?;
         Ok(())
     }
 
+    /// Get an arbitrary field from the manifest.
     pub fn get_field(&self, key: &str) -> Option<&Value> {
         self.data.get(key)
     }
 
+    /// Set an arbitrary field from the manifest.
     pub fn set_field(&mut self, key: impl Into<String>, value: Value) {
         self.data.insert(key.into(), value);
     }
 
-    pub fn author(&self) -> Result<&str, WeirpackError> {
-        self.required_str("author")
-    }
+    gen_fns!(author);
+    gen_fns!(version);
+    gen_fns!(description);
+    gen_fns!(license);
 
-    pub fn set_author(&mut self, value: impl Into<String>) {
-        self.set_field("author", Value::String(value.into()));
-    }
-
-    pub fn version(&self) -> Result<&str, WeirpackError> {
-        self.required_str("version")
-    }
-
-    pub fn set_version(&mut self, value: impl Into<String>) {
-        self.set_field("version", Value::String(value.into()));
-    }
-
-    pub fn description(&self) -> Result<&str, WeirpackError> {
-        self.required_str("description")
-    }
-
-    pub fn set_description(&mut self, value: impl Into<String>) {
-        self.set_field("description", Value::String(value.into()));
-    }
-
-    pub fn license(&self) -> Result<&str, WeirpackError> {
-        self.required_str("license")
-    }
-
-    pub fn set_license(&mut self, value: impl Into<String>) {
-        self.set_field("license", Value::String(value.into()));
-    }
-
-    fn required_str(&self, key: &'static str) -> Result<&str, WeirpackError> {
+    fn required_str(&self, key: &'static str) -> Result<&str, Error> {
         match self.data.get(key) {
             Some(Value::String(value)) => Ok(value),
-            Some(_) => Err(WeirpackError::InvalidManifestFieldType(key)),
-            None => Err(WeirpackError::MissingManifestField(key)),
+            Some(_) => Err(Error::InvalidManifestFieldType(key)),
+            None => Err(Error::MissingManifestField(key)),
         }
     }
 
-    fn validate_required(&self) -> Result<(), WeirpackError> {
+    fn validate_required(&self) -> Result<(), Error> {
         self.author()?;
         self.version()?;
         self.description()?;
