65e67b75498dc868dc104dbb4d4788dc8780c7cb
Andrew Dunbar <hippytrail@gmail.com>
2026-02-13 04:11:58 +0700

    fix: add Zig to places I missed on the previous PR (#2709)
    * fix: add Zig to places I missed on the previous PR
    
    * fix: comment out Zig test as is done for cmake, haskell, nix, toml, typst, solitidy

--- packages/vscode-plugin/src/tests/suite/languages.test.ts
+++ packages/vscode-plugin/src/tests/suite/languages.test.ts
@@ -24,6 +24,7 @@ describe('Languages >', () => {
 		// { type: 'TOML', file: 'toml.toml', row: 1, column: 2 },
 		// { type: 'Typst', file: 'typst.typ', row: 2, column: 1 },
 		// { type: 'Solidity', file: 'solidity.sol', row: 3, column: 4 },
+		// { type: 'Zig', file: 'zig.zig', row: 0, column: 2 },
 
 		{ type: 'C', file: 'c.c', row: 2, column: 3 },
 		{ type: 'C++', file: 'cpp.cpp', row: 3, column: 5 },
