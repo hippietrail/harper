e869b3af55facd6433e379af95373af915123df6
Elijah Potter <me@elijahpotter.dev>
2026-02-13 08:57:31 -0700

    feat(harper-comments): add Groovy support (#2713)
    * feat(harper-comments): add Groovy support
    
    * feat: add mentions of Groovy support in other places

--- packages/vscode-plugin/src/tests/suite/languages.test.ts
+++ packages/vscode-plugin/src/tests/suite/languages.test.ts
@@ -33,6 +33,7 @@ describe('Languages >', () => {
 		{ type: 'Dart', file: 'dart.dart', row: 1, column: 29 },
 		{ type: 'Git Commit', file: 'git-commit', row: 0, column: 0 },
 		{ type: 'Go', file: 'go.go', row: 4, column: 4 },
+		{ type: 'Groovy', file: 'groovy.groovy', row: 0, column: 3 },
 		{ type: 'HTML', file: 'html.html', row: 8, column: 6 },
 		{ type: 'Java', file: 'java.java', row: 2, column: 17 },
 		{ type: 'JavaScript', file: 'javascript.js', row: 1, column: 3 },
