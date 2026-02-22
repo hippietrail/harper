40b10cfb6918d731cc30185180d6f67fdf8b0d52
hippietrail <hippytrail@gmail.com>
2026-02-22 15:40:44 +0700

    chore: finished do mistake→make mistake

--- harper-core/src/linting/do_mistake.rs
+++ harper-core/src/linting/do_mistake.rs
@@ -1,7 +1,7 @@
 use crate::{
-    Lint, Token,
+    CharStringExt, Lint, Token,
     expr::{Expr, FixedPhrase, SequenceExpr},
-    linting::{ExprLinter, expr_linter::Chunk},
+    linting::{ExprLinter, LintKind, Suggestion, expr_linter::Chunk},
     patterns::WordSet,
 };
 
@@ -15,10 +15,10 @@ impl Default for DoMistake {
             expr: Box::new(
                 SequenceExpr::word_set(&["do", "did", "does", "doing", "done"])
                     .t_ws()
-                    .then_any_of(vec![
+                    .then_longest_of(vec![
                         Box::new(WordSet::new(&[
                             "a", "an", "the", "that", "these", "this", "those", "another", "many",
-                            "my", "our", "your", "his", "her", "its", "their",
+                            "several", "some", "my", "our", "your", "his", "her", "its", "their",
                         ])),
                         Box::new(FixedPhrase::from_phrase("a lot of")),
                         Box::new(FixedPhrase::from_phrase("lots of")),
@@ -47,15 +47,44 @@ impl ExprLinter for DoMistake {
         src: &[char],
         ctx: Option<(&[Token], &[Token])>,
     ) -> Option<Lint> {
-        eprintln!(
-            "💥 {}",
-            crate::linting::debug::format_lint_match(toks, ctx, src)
-        );
-        None
+        let tok = toks.first()?;
+        let span = tok.span;
+        let chars = span.get_content(src);
+
+        if let Some((_, after)) = ctx
+            && let [ws, v, ..] = after
+            && ws.kind.is_whitespace()
+            && v.kind.is_verb()
+            && !v.kind.is_verb_progressive_form()
+        {
+            return None;
+        }
+
+        let make = if chars.eq_ignore_ascii_case_str("do") {
+            "make"
+        } else if chars.eq_ignore_ascii_case_str("did") || chars.eq_ignore_ascii_case_str("done") {
+            "made"
+        } else if chars.eq_ignore_ascii_case_str("does") {
+            "makes"
+        } else if chars.eq_ignore_ascii_case_str("doing") {
+            "making"
+        } else {
+            return None;
+        }
+        .chars()
+        .collect();
+
+        Some(Lint {
+            span,
+            lint_kind: LintKind::Usage,
+            suggestions: vec![Suggestion::replace_with_match_case(make, chars)],
+            message: "In English we `make` mistakes, not `do` them".to_string(),
+            ..Default::default()
+        })
     }
 
     fn description(&self) -> &str {
-        "Corrects `do a mistake` to `make a mistake`"
+        "Corrects `do a mistake` to `make a mistake`."
     }
 
     fn expr(&self) -> &dyn Expr {
@@ -66,168 +95,176 @@ impl ExprLinter for DoMistake {
 #[cfg(test)]
 mod tests {
     use super::DoMistake;
-    use crate::linting::tests::{assert_lint_count, assert_no_lints};
+    use crate::linting::tests::{assert_lint_count, assert_no_lints, assert_suggestion_result};
 
     #[test]
     fn did_a_mistake() {
-        assert_lint_count(
+        assert_suggestion_result(
             "Hi, I did a mistake in my NGINX config file and so once the container is launched, it logs the error...",
             DoMistake::default(),
-            1,
+            "Hi, I made a mistake in my NGINX config file and so once the container is launched, it logs the error...",
         );
     }
 
     #[test]
     fn did_my_mistakes() {
-        assert_lint_count("Where i did my mistakes?", DoMistake::default(), 1);
+        assert_suggestion_result(
+            "Where i did my mistakes?",
+            DoMistake::default(),
+            "Where i made my mistakes?",
+        );
     }
 
     #[test]
     fn did_several_mistakes() {
-        assert_lint_count(
+        assert_suggestion_result(
             "Maybe I did several mistakes, but I can only find a message about one?",
             DoMistake::default(),
-            1,
+            "Maybe I made several mistakes, but I can only find a message about one?",
         );
     }
 
     #[test]
     fn did_some_mistakes() {
-        assert_lint_count(
+        assert_suggestion_result(
             "I made this program to learn goto use. but did some mistakes somewhere",
             DoMistake::default(),
-            1,
+            "I made this program to learn goto use. but made some mistakes somewhere",
         );
     }
 
     #[test]
     fn did_that_mistake() {
-        assert_lint_count(
+        assert_suggestion_result(
             "and believe me, I did that mistake too",
             DoMistake::default(),
-            1,
+            "and believe me, I made that mistake too",
         );
     }
 
     #[test]
     fn did_the_mistake() {
-        assert_lint_count(
+        assert_suggestion_result(
             "The issue describe is the person who did the mistake in the past & that same person is NOW correcting other people",
             DoMistake::default(),
-            1,
+            "The issue describe is the person who made the mistake in the past & that same person is NOW correcting other people",
         );
     }
 
     #[test]
     fn did_this_mistake() {
-        assert_lint_count(
+        assert_suggestion_result(
             "Are there famous mathematicians who did this mistake?",
             DoMistake::default(),
-            1,
+            "Are there famous mathematicians who made this mistake?",
         );
     }
 
     #[test]
     fn do_many_mistakes() {
-        assert_lint_count(
+        assert_suggestion_result(
             "I observed that my coworkers do many mistakes using the field calculator",
             DoMistake::default(),
-            1,
+            "I observed that my coworkers make many mistakes using the field calculator",
         );
     }
 
     #[test]
     fn do_mistake() {
-        assert_lint_count(
+        assert_suggestion_result(
             "If you do a mistake that causes alot of problems, please use the command to redo",
             DoMistake::default(),
-            1,
+            "If you make a mistake that causes alot of problems, please use the command to redo",
         );
     }
 
     #[test]
     fn do_some_mistakes() {
-        assert_lint_count(
+        assert_suggestion_result(
             "so probably if my colleagues do some mistakes I tend to learn them as well",
             DoMistake::default(),
-            1,
+            "so probably if my colleagues make some mistakes I tend to learn them as well",
         );
     }
 
     #[test]
     fn do_the_mistake() {
-        assert_lint_count(
+        assert_suggestion_result(
             "do I need to explicitly mention that I did not do the mistake to do not lose the point?",
             DoMistake::default(),
-            1,
+            "do I need to explicitly mention that I did not make the mistake to do not lose the point?",
         );
     }
 
     #[test]
     fn do_this_mistake() {
-        assert_lint_count(
+        assert_suggestion_result(
             "I barely remember any frontend developer that wouldn't do this mistake at least once.",
             DoMistake::default(),
-            1,
+            "I barely remember any frontend developer that wouldn't make this mistake at least once.",
         );
     }
 
     #[test]
     fn do_this_mistakes() {
-        assert_lint_count(
+        assert_suggestion_result(
             "I do this mistakes to check the command detekt with type resolution",
             DoMistake::default(),
-            1,
+            "I make this mistakes to check the command detekt with type resolution",
         );
     }
 
     #[test]
     fn do_those_mistakes() {
-        assert_lint_count(
+        assert_suggestion_result(
             "An experienced developer could do those mistakes as well",
             DoMistake::default(),
-            1,
+            "An experienced developer could make those mistakes as well",
         );
     }
 
     #[test]
     fn doing_a_mistake() {
-        assert_lint_count(
+        assert_suggestion_result(
             "Here at work, a colleague asked if we were doing a mistake by using the ReactDOM.renderToStaticMarkup on the client side.",
             DoMistake::default(),
-            1,
+            "Here at work, a colleague asked if we were making a mistake by using the ReactDOM.renderToStaticMarkup on the client side.",
         );
     }
 
     #[test]
     fn doing_several_mistakes() {
-        assert_lint_count(
+        assert_suggestion_result(
             "I realized I was doing several mistakes",
             DoMistake::default(),
-            1,
+            "I realized I was making several mistakes",
         );
     }
 
     #[test]
-    fn doing_the_mistake() {
-        assert_lint_count("where am i doing the mistake?", DoMistake::default(), 1);
+    fn doing_the_mistkae() {
+        assert_suggestion_result(
+            "where am i doing the mistake?",
+            DoMistake::default(),
+            "where am i making the mistake?",
+        );
     }
 
     #[test]
     fn done_some_mistake() {
-        assert_lint_count(
+        assert_suggestion_result(
             "Might be I have done some mistake, that I do not know.",
             DoMistake::default(),
-            1,
+            "Might be I have made some mistake, that I do not know.",
         );
     }
 
     #[test]
     fn done_this_mistake() {
-        assert_lint_count(
+        assert_suggestion_result(
             "how many more users have done this mistake?",
             DoMistake::default(),
-            1,
+            "how many more users have made this mistake?",
         );
     }
 
