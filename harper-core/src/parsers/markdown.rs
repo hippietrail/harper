0ee8d01b13d7170c2e70f2be59b3354d00519f99
hippietrail <hippytrail@gmail.com>
2026-04-16 22:13:47 +0700

    fix: clippy now wants `while let`

--- harper-core/src/parsers/markdown.rs
+++ harper-core/src/parsers/markdown.rs
@@ -54,11 +54,7 @@ impl Markdown {
             let mut cursor = pipe_idx - 2;
             let mut open_bracket = None;
 
-            loop {
-                let Some((a, b)) = tokens.get(cursor).zip(tokens.get(cursor + 1)) else {
-                    break;
-                };
-
+            while let Some((a, b)) = tokens.get(cursor).zip(tokens.get(cursor + 1)) {
                 if a.kind.is_newline() {
                     break;
                 }
@@ -77,11 +73,7 @@ impl Markdown {
             cursor = pipe_idx + 1;
             let mut close_bracket = None;
 
-            loop {
-                let Some((a, b)) = tokens.get(cursor).zip(tokens.get(cursor + 1)) else {
-                    break;
-                };
-
+            while let Some((a, b)) = tokens.get(cursor).zip(tokens.get(cursor + 1)) {
                 if a.kind.is_newline() {
                     break;
                 }
