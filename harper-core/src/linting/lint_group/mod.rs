6457764429c7bcae4e8745d3d217317e33e349db
Andrew Dunbar <hippytrail@gmail.com>
2026-07-28 02:06:42 +0800

    feat: filled/full to the brim of → with (#3898)
    * feat: filled/full to the brim of → with
    
    * fix: remove debug code
    
    * fix: unused crates

--- harper-core/src/linting/lint_group/mod.rs
+++ harper-core/src/linting/lint_group/mod.rs
@@ -102,6 +102,7 @@ use super::for_noun::ForNoun;
 use super::for_the_nth_time::ForTheNthTime;
 use super::free_predicate::FreePredicate;
 use super::friend_of_me::FriendOfMe;
+use super::full_to_the_brim::FullToTheBrim;
 use super::go_so_far_as_to::GoSoFarAsTo;
 use super::go_to_war::GoToWar;
 use super::good_at::GoodAt;
@@ -689,6 +690,7 @@ impl LintGroup {
         insert_expr_rule!(ForTheNthTime);
         insert_expr_rule!(FreePredicate);
         insert_expr_rule!(FriendOfMe);
+        insert_expr_rule!(FullToTheBrim);
         insert_expr_rule!(GoSoFarAsTo);
         insert_expr_rule!(GoToWar);
         insert_expr_rule!(GoodAt);
