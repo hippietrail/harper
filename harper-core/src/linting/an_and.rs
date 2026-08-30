dafd065a8fffb627fea798968b15d399639839b8
hippietrail <hippytrail@gmail.com>
2026-08-31 00:55:34 +0800

    fix: iterate logic

--- harper-core/src/linting/an_and.rs
+++ harper-core/src/linting/an_and.rs
@@ -1,19 +1,20 @@
 use crate::{
     CharStringExt, Lint, Token,
+    char_ext::CharExt,
     expr::{Expr, FirstMatchOf, SequenceExpr},
     linting::{
         ExprLinter, LintKind, Suggestion,
         debug::format_lint_match,
-        expr_linter::{Chunk, find_the_only_token_matching, followed_by_word},
+        expr_linter::{Chunk, find_the_only_token_matching},
     },
 };
 
-// also any conjunction??
+// Note: MUST be lowercased! also any conjunction??
 const NOT_USUALLY_BEFORE_AND: &[&str] = &[
-    "and", "as", "by", "for", "is", "of", "on", "to", "was", "with",
+    "and", "as", "by", /* "for", */ "is", "of", "on", "to", "was", "with",
 ];
 
-// also any Pronoun, Determiner, Preposition, Conjunction, Adverb??
+// Note: MUST be lowercased! also any Pronoun, Determiner, Preposition, Conjunction, Adverb??
 const NOT_USUALLY_AFTER_AN: &[&str] = &[
     "a",     // det.indef
     "his",   // det.poss
@@ -22,7 +23,7 @@ const NOT_USUALLY_AFTER_AN: &[&str] = &[
     "the",   // det.def
     "their", // det.poss,
     "he",    // pron.subj
-    "I",     // pron subj.
+    "i",     // pron subj.
     "it",    // pron.subj+obj
     "you",   // pron.subj+obj
     "in",    // prep
@@ -62,6 +63,7 @@ impl ExprLinter for AnAnd {
         ctx: Option<(&[Token], &[Token])>,
     ) -> Option<Lint> {
         eprintln!("🚨 {}", format_lint_match(toks, ctx, src));
+
         let span = find_the_only_token_matching(toks, src, |t: &Token, s: &[char]| {
             t.get_ch(s)
                 .eq_any_ignore_ascii_case_chars(&[&['a', 'n'], &['a', 'n', 'd']])
@@ -70,12 +72,72 @@ impl ExprLinter for AnAnd {
 
         let ch = span.get_content(src);
 
-        if ch.eq_ch(&['a', 'n', 'd']) {
-            if toks[0].get_ch(src).eq_ch(&['o', 'n'])
-                && followed_by_word(ctx, |next| next.get_ch(src).eq_ch(&['o', 'f', 'f']))
-            {
-                return None;
+        // if ch is 'an' we already have the next word in toks, so get the previous word from ctx
+        // if ch is 'and' we already have the prev word in toks, so get the next word from ctx
+        match ch.len() {
+            2 => {
+                // got 'an' - change to 'and'?
+
+                let maybe_prev = if let Some((before, _)) = ctx
+                    && let [ws, word, ..] = before
+                    && ws.kind.is_whitespace()
+                {
+                    Some(word)
+                } else {
+                    None
+                };
+
+                let next_tok = &toks[2];
+                let next_ch = next_tok.get_ch(src);
+
+                // don't change it to 'and' if the previous word is one that doesn't usually come before 'and'
+                if maybe_prev.is_some_and(|t| {
+                    t.get_ch(src)
+                        .eq_any_ignore_ascii_case_str(NOT_USUALLY_BEFORE_AND)
+                }) {
+                    return None;
+                }
             }
+            3 => {
+                // got 'and' - change to 'an'?
+
+                let prev_tok = &toks[0];
+                let prev_ch = prev_tok.get_ch(src);
+
+                let maybe_next = if let Some((_, after)) = ctx
+                    && let [ws, word, ..] = after
+                    && ws.kind.is_whitespace()
+                {
+                    Some(word)
+                } else {
+                    None
+                };
+
+                // "an" only comes before a vowel sound, not a consonant sound
+                if maybe_next.is_some_and(|w| w.get_ch(src).first().is_some_and(|c| !c.is_vowel()))
+                {
+                    eprintln!("Skipping 'an' because next word starts with a consonant sound");
+                    return None;
+                }
+
+                // don't change it to 'an' if the next word is one that doesn't usually come after 'an'
+                if maybe_next.is_some_and(|t| {
+                    t.get_ch(src)
+                        .eq_any_ignore_ascii_case_str(NOT_USUALLY_AFTER_AN)
+                }) {
+                    eprintln!("Skipping 'an' because next word is not usually after 'an'");
+                    return None;
+                }
+
+                // don't change it to 'an' if the previous word is 'on' and the next word is 'off'
+                if prev_ch.eq_ch(&['o', 'n'])
+                    && maybe_next.is_some_and(|w| w.get_ch(src).eq_ch(&['o', 'f', 'f']))
+                {
+                    eprintln!("Skipping 'an' because previous word is 'on' and next word is 'off'");
+                    return None;
+                }
+            }
+            _ => return None,
         }
 
         let mut the_word: Vec<char> = ch.to_vec();
